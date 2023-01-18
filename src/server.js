90d9842b0c4aa1562f9ca43a97bb2a724e02c84f
Renato Santos <renato_jfs@hotmail.com>
2023-01-17 23:37:42 -0300

    Feat: Request body in JSON (Stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -2,26 +2,39 @@ import http from 'node:http';
 
 const users = []
 
-const server = http.createServer((request, response) => {
-  const { method, url } = request
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/users') {
-    return response
+    return res
     .setHeader('Content-Type', 'application/json')
     .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
     users.push({
       id: 1,
-      name: 'Fulano',
-      email: 'fulano@exemplo.com'
+      name,
+      email,
     })
 
-    return response.writeHead(201).end()
+    return res.writeHead(201).end()
   }
 
-  return response.writeHead(404).end()
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
